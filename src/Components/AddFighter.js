222e91ea94e90a4e8338daad9536248980b20992
George Atkinson <georgeatkinsonwd@gmail.com>
2022-07-18 15:01:34 +0100

    cleared the form after submission

--- src/Components/AddFighter.js
+++ src/Components/AddFighter.js
@@ -4,7 +4,6 @@ import { useState } from 'react'
 
 
 const AddFighter = () => {
-    const [listOfFighters, setListOfFighters] = useState([])
     const [name, setName] = useState("")
     const [record, setRecord] = useState("")
     const [age, setAge] = useState(0)
@@ -12,7 +11,7 @@ const AddFighter = () => {
     const [weightClass, setWeightClass] = useState("")
     const [reach, setReach] = useState("")
 
-    const createFighter = () => {
+    const createFighter = async () => {
         Axios.post("http://localhost:3001/createFighter", {
           name,
           record,
@@ -21,31 +20,35 @@ const AddFighter = () => {
           weightClass,
           reach,
           }).then((response)=>{
-          setListOfFighters([...listOfFighters, {
-            name,
-            record,
-            age,
-            height,
-            weightClass,
-            reach,
-          }
-        ])
+            
         alert('fighter added to database')
+        
         })
-    
+        
+        
       }
-
+        const onHandleSubmit = (e) => {
+            e.preventDefault()
+            setName("")
+            setRecord("")
+            setAge("0")
+            setHeight("")
+            setWeightClass("")
+            setReach("")
+        }
     return(
         <div>
             <h1>Add a fighter to Database</h1>
             <div>
-          <input type="text" placeholder='name' onChange={(e)=> { setName(e.target.value)}}></input>
-          <input type="text" placeholder='record' onChange={(e)=> { setRecord(e.target.value)}}></input>
-          <input type="number" placeholder='age' onChange={(e)=> { setAge(e.target.value)}}></input>
-          <input type="text" placeholder='height' onChange={(e)=> { setHeight(e.target.value)}}></input>
-          <input type="text" placeholder='Weightclass' onChange={(e)=> { setWeightClass(e.target.value)}}></input>
-          <input type="text" placeholder='reach'onChange={(e)=> { setReach(e.target.value)}}></input>
-          <button onClick={createFighter}>Add Fighter</button>
+        <form onSubmit={onHandleSubmit}>
+          <input value={name} type="text" placeholder='name' onChange={(e)=> { setName(e.target.value)}}></input>
+          <input value={record} type="text" placeholder='record' onChange={(e)=> { setRecord(e.target.value)}}></input>
+          <input value={age} type="number" placeholder='age' onChange={(e)=> { setAge(e.target.value)}}></input>
+          <input value={height} type="text" placeholder='height' onChange={(e)=> { setHeight(e.target.value)}}></input>
+          <input value={weightClass} type="text" placeholder='Weightclass' onChange={(e)=> { setWeightClass(e.target.value)}}></input>
+          <input value={reach} type="text" placeholder='reach'onChange={(e)=> { setReach(e.target.value)}}></input>
+          <button type="submit" onClick={createFighter}>Add Fighter</button>
+          </form>
 
         </div>
         </div>
